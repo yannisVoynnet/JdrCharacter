fa4e904a71e97cc53bd697be047453d0b0e09fb0
yannisVoynnet <yvoynnet@gmail.com>
2024-06-03 16:07:26 -0400

    Initial Commit

---
+++ src/main/java/com/example/JdrCharacter/JdrCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.example.JdrCharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdrCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdrCharacterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JdrCharacter/JdrCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JdrCharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdrCharacterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
